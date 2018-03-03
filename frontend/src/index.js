8288453c1c5dd10ca0f3ef093b22c7edbc6d38fd
Mark McConnell <mjmcconnell.dev@gmail.com>
2018-03-03 18:33:35 +0000

    dynamic fields setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,149 +2,118 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// eslint-disable-next-line
+var instruments_json = [{
+    "status": "acpt",
+    "fields": [
+        {
+            "id": "tec_id",
+            "label": "Tech record ID",
+            "description": "",
+            "value": "AS124124E32-ASDFASDFA",
+        },
+        {
+            "id": "name",
+            "label": "Name",
+            "description": "Instrument name",
+            "value": "First",
+        },
+    ],
+    "errors": [
+        {"id": "AAA-10", "desc": "Whoops"},
+        {"id": "ZZZ-10", "desc": "Another oops"},
+    ],
+}]
 
-function Square(props) {
+function InstrumentTableBodyRowCell(props) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
+      <td
+        className="review-table-body-row-field review-table-body-row-field-{props.field.field_type}"
+      >
+        {props.field.value}
+      </td>
+    )
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+function InstrumentTableBodyRowStatusCell(props) {
+  return <td className="review-table-body-row-field-status-{props.status}"></td>
 }
 
-class Game extends React.Component {
+function InstrumentTableBodyRow(props) {
+  return (
+    <tr className="review-table-body-row">
+      <InstrumentTableBodyRowStatusCell status={props.instrument.status} />
+      {props.instrument.fields.map(function(field, key) {
+        return <InstrumentTableBodyRowCell field={field}/>
+      })}
+    </tr>
+  )
+}
+
+function InstrumentTableBody(props) {
+  return (
+    <tbody className="review-table-body">
+      {props.instruments.map(function(instrument, key) {
+        return <InstrumentTableBodyRow instrument={instrument} />
+      })}
+    </tbody>
+  )
+}
+
+function InstrumentTableHeadRowCell(props) {
+  return (
+    <th className="review-table-head-row-column review-table-head-row-heading-{{props.field_type}}">
+      <span>{props.heading}</span>
+    </th>
+  )
+}
+
+function InstrumentTableHead(props) {
+  return (
+    <thead className="review-table-head">
+      <tr className="review-table-head-row">
+        <th></th>
+        {props.headings.map(function(heading, key) {
+          return <InstrumentTableHeadRowCell heading={heading}/>
+        })}
+      </tr>
+    </thead>
+  )
+}
+
+class InstrumentTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      xIsNext: true,
-      stepNumber: 0
+      report: {
+        headings: ["Tech record ID", "Name"],
+        instruments: instruments_json,
+      },
     };
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
+    // Sets the active insturment for the modal, and triggers modal
+    const instruments = this.state.report.instruments.slice();
+    this.setState({activeInstrument: instruments[i]})
   }
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-
     return (
-      <div className="game">
-        <div className="game-board">
-           <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
+      <table className="review-table">
+        <InstrumentTableHead
+            headings={this.state.report.headings}
+        />
+        <InstrumentTableBody
+          instruments={this.state.report.instruments}
+          onClick={(i) => this.handleClick(i)}
+        />
+      </table>
+    )
   }
 }
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <InstrumentTable />,
   document.getElementById('root')
 );
